fd977c49aaf0b16c84f8fd7b41b181271ab87c5b
Regionald <regionaldmongwe@gmail.com>
2021-11-04 14:38:06 +0200

    added query table

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let app = express();
 
 let fav = require('serve-favicon');
 
-var moment = require('moment'); 
+var moment = require('moment');
 
 const exphbs = require('express-handlebars');
 
@@ -48,65 +48,82 @@ open({
   app.get('', (req, res) => {
     res.render('image');
   });
+  // list of querries 
+  app.get('/admin', (req, res) => {
+
+    var orders=[{lat: 'hhh',
+    long: 'dd',
+    name: 'pretoria'},
+    {lat: 'hhh',
+    long: 'dd',
+    name: 'joburg'},{lat: 'hhh',
+    long: 'dd',
+    name: 'bush'}
+    
+  ];
+
+    res.render('querry', {orders:orders});
+  });
+
   app.post('', (req, res) => {
     let sampleFile;
     let uploadPath;
-  
+
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send('No files were uploaded.');
     }
     sampleFile = req.files.sampleFile;
     console.log(sampleFile);
 
-  app.post('/login', async (req, res) => {
-    req.session.email = req.body.email;
-    req.session.psw = req.body.psw;
-    let sql = await db.get('Select Email email, Password psw from signup where Email = ?', req.session.email);
-    console.log(sql)
-    if (sql == null) {
-      console.log('Incorrect Email or password');
-      res.redirect('/');
-    }
-    if (sql.psw !== req.session.psw) {
-      console.log('Incorrect Email or password')
-      res.redirect('/')
-    }
-    else {
-      res.redirect('/')
-    }
-
-  });
-  app.post('/register', async (req, res) => {
-    const { name, email, psw, psw1, user_type } = req.body;
-
-    req.session.name = name;
-    req.session.email = email;
-    req.session.psw = psw;
-    req.session.psw1 = psw1;
-    req.session.user_type = user_type;
-    let sql = await db.get('Select Email email, Password psw from signup where Email = ?', req.session.email);
-    if (sql == null) {
-      if (req.session.psw == psw1) {
-        const insert_details = 'insert into signup (name, email, password, type_of_user) values (?, ?, ?, ?)';
-        await db.run(insert_details, req.session.name, req.session.email, req.session.psw, req.session.user_type);
+    app.post('/login', async (req, res) => {
+      req.session.email = req.body.email;
+      req.session.psw = req.body.psw;
+      let sql = await db.get('Select Email email, Password psw from signup where Email = ?', req.session.email);
+      console.log(sql)
+      if (sql == null) {
+        console.log('Incorrect Email or password');
         res.redirect('/');
+      }
+      if (sql.psw !== req.session.psw) {
+        console.log('Incorrect Email or password')
+        res.redirect('/')
       }
       else {
         res.redirect('/')
       }
-    }
-    else {
-      //Write a message to alert the user that the email they using is already in the system
-      res.redirect('/')
-    }
+
+    });
+    app.post('/register', async (req, res) => {
+      const { name, email, psw, psw1, user_type } = req.body;
+
+      req.session.name = name;
+      req.session.email = email;
+      req.session.psw = psw;
+      req.session.psw1 = psw1;
+      req.session.user_type = user_type;
+      let sql = await db.get('Select Email email, Password psw from signup where Email = ?', req.session.email);
+      if (sql == null) {
+        if (req.session.psw == psw1) {
+          const insert_details = 'insert into signup (name, email, password, type_of_user) values (?, ?, ?, ?)';
+          await db.run(insert_details, req.session.name, req.session.email, req.session.psw, req.session.user_type);
+          res.redirect('/');
+        }
+        else {
+          res.redirect('/')
+        }
+      }
+      else {
+        //Write a message to alert the user that the email they using is already in the system
+        res.redirect('/')
+      }
+    });
+
   });
 
-});
 
+  let PORT = process.env.PORT || 3001;
 
-let PORT = process.env.PORT || 3001;
-
-app.listen(PORT, function () {
-  console.log('App starting on port', PORT);
-});
+  app.listen(PORT, function () {
+    console.log('App starting on port', PORT);
+  });
 });
