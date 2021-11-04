0010325ddab24577cd5fd73b9c251dc72df5a08b
christian <cktshukudu@gmail.com>
2021-11-04 03:06:26 +0200

    added handlebars and image functionality

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let express = require('express');
-<<<<<<< HEAD
+const fileupload = require('express-fileupload');
+// <<<<<<< HEAD
 //Regionald
 
-=======
-const what = "mistake";
->>>>>>> f74d3e48b0f7b527a2017b79ddef35a7ddbbd23e
+// =======
+// const what = "mistake";
+// >>>>>>> f74d3e48b0f7b527a2017b79ddef35a7ddbbd23e
 let app = express();
 
 let fav = require('serve-favicon');
@@ -27,28 +28,37 @@ const { compile } = require('handlebars');
 
 //Set-up middleware
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 300000 } }))
-app.use(fav(path.join(__dirname, 'public', 'img/favicon.ico')))
+//app.use(fav(path.join(__dirname, 'public', 'img/favicon.ico')))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'));
 
-open({
-  filename: './data.db',
-  driver: sqlite3.Database
-}).then(async function (db) {
+// open({
+//   filename: './data.db',
+//   driver: sqlite3.Database
+// }).then(async function (db) {
 
   // run migrations
 
-  await db.migrate();
+  // await db.migrate();
 
   // only setup the routes once the database connection has been established
-  app.get('/', (req, res) => {
+  app.get('', (req, res) => {
+    res.render('image');
+  });
+  app.post('', (req, res) => {
+    let sampleFile;
+    let uploadPath;
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+    sampleFile = req.files.sampleFile;
+    console.log(sampleFile);
 
-    res.render('home');
   });
 
-
-});
+// });
 
 
 let PORT = process.env.PORT || 3001;
