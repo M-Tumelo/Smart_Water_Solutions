7aea164f4568d8dc11674d712071ccec194c9410
christian <cktshukudu@gmail.com>
2021-11-10 06:20:46 +0200

    added fetch method to get location

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let express = require('express');
-const fileupload = require('express-fileupload');
+const fileUpload = require('express-fileupload');
+
+
 
 let app = express();
 
@@ -15,8 +17,6 @@ const path = require('path');
 //import sqlite modules
 const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
-var cors = require('cors');
-app.use(cors());
 
 //Configure the express-handlebars module
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -37,80 +37,55 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'));
-
-open({
-  filename: './data.db',
+app.use(fileUpload());
+app.use(express.json({limit: '5mb' }));
+ open({
+   filename: './data.db',
   driver: sqlite3.Database
 }).then(async function (db) {
 
   // run migrations
 
+
   await db.migrate();
 
+
   // only setup the routes once the database connection has been established
-  app.get('', (req, res) => {
+  app.get('/user', (req, res) => {
     res.render('image');
   });
 
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/register', (req, res)=>{
+  res.render('home');
+})
+
+app.get('/login', (req, res)=>{
+  res.render('home');
+})
+
   // list of querries 
-  app.get('/data', (req, res) => {
-    const geojson = {
-      type: 'FeatureCollection',
-      features: [
-        {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [28.2292712, -25.7478676]
-          },
-          properties: {
-            title: 'Mapbox',
-            description: 'Home'
-          }
-        },
-        {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [28.229271, -28.7478676]
-          },
-          properties: {
-            title: 'Mapbox',
-            description: 'this Is so Funny'
-          }
-        },
-        {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [28.229271, -26.7478676]
-          },
-          properties: {
-            title: 'Mapbox',
-            description: 'point number 3'
-          },
-        },
-        {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [29.229271, -26.7478676]
-          },
-          properties: {
-            title: 'Mapbox',
-            description: 'point number 4'
-          },
-        }
-      ]
-    };
-    res.json(geojson);
-  })
   app.get('/admin', (req, res) => {
 
-    res.render('querry');
+    var orders=[{lat: 'hhh',
+    long: 'dd',
+    name: 'pretoria'},
+    {lat: 'hhh',
+    long: 'dd',
+    name: 'joburg'},{lat: 'hhh',
+    long: 'dd',
+    name: 'bush'}
+    
+  ];
+
+    res.render('querry', {orders:orders});
   });
 
-  app.post('', (req, res) => {
+  app.post('/user', (req, res) => {
+
     let sampleFile;
     let uploadPath;
 
@@ -118,15 +93,60 @@ open({
       return res.status(400).send('No files were uploaded.');
     }
     sampleFile = req.files.sampleFile;
-    console.log(sampleFile);
+    uploadPath = __dirname + '/upload/' + sampleFile.name;
+    //console.log(sampleFile);
 
-    app.post('/login', async (req, res) => {
-      req.session.email = req.body.email;
-      req.session.psw = req.body.psw;
-      let sql = await db.get('Select Email email, Password psw from signup where Email = ?', req.session.email);
-      console.log(sql)
-      if (sql == null) {
-        console.log('Incorrect Email or password');
+    sampleFile.mv(uploadPath, function (err) {
+      if(err) return res.status(500).send(err);
+      res.send('File uploaded');
+      });
+      
+      
+  });
+
+  // app.post('/fetch', (req, res) => {
+  //   console.log(req.body);
+  //   const position = req.body;
+  //   res.json({
+  //     status: 'Success',
+  //     latitude: position.latitude,
+  //     longitude: po.longitude
+  //   });
+  // });
+
+  app.post('/login', async (req, res) => {
+    req.session.email = req.body.email;
+    req.session.psw = req.body.psw;
+    let sql = await db.get('Select * from signup where Email = ?', req.session.email);
+    console.log(sql)
+    if (sql == null) {
+      console.log('Incorrect Email or password');
+      res.redirect('/');
+    }
+    if (sql.password !== req.session.psw) {
+      console.log('Incorrect Email or password')
+      res.redirect('/')
+    }
+    else {
+      if(sql.type_of_user == 'user') res.redirect('/user');
+      else res.redirect('/technician');
+    }
+  });
+
+
+  app.post('/register', async (req, res) => {
+    const { name, email, psw, psw1, user_type } = req.body;
+
+    req.session.name = name;
+    req.session.email = email;
+    req.session.psw = psw;
+    req.session.psw1 = psw1;
+    req.session.user_type = user_type;
+    let sql = await db.get('Select Email email, Password psw from signup where Email = ?', req.session.email);
+    if (sql == null) {
+      if (req.session.psw == psw1) {
+        const insert_details = 'insert into signup (name, email, password, type_of_user) values (?, ?, ?, ?)';
+        await db.run(insert_details, req.session.name, req.session.email, req.session.psw, req.session.user_type);
         res.redirect('/');
       }
       if (sql.psw !== req.session.psw) {
@@ -136,7 +156,7 @@ open({
       else {
         res.redirect('/')
       }
-
+    }
     });
     app.post('/register', async (req, res) => {
       const { name, email, psw, psw1, user_type } = req.body;
@@ -162,13 +182,10 @@ open({
         res.redirect('/')
       }
     });
-
   });
-
 
   let PORT = process.env.PORT || 3001;
 
   app.listen(PORT, function () {
     console.log('App starting on port', PORT);
   });
-});
